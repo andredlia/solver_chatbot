41893a4d03e2a749e378da2eddffe626397bfec3
Andrea-B3zz <andreabezzolato@MacBook-Pro-di-Andrea.local>
2025-04-14 17:33:44 +0200

    Eslint updated

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false
-  /* config options here */
+  devIndicators: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  /* other config options here */
 };
 
 export default nextConfig;
